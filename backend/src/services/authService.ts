672d3bb370f20d91fe0c1e46e6fffc2b1f66dd57
Sergey <sergey_chizhik@ukr.net>
2021-03-05 10:06:05 +0200

    Update authService.ts

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,12 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createAccessToken, createRefreshToken } from '../common/helpers/tokenHelper';
 import { encrypt } from '../common/helpers/cryptoHelper';
-import { IUser } from '../common/models/user/user';
 import * as userRepository from '../data/repositories/userRepository';
+import { IUser } from '../common/models/user/user';
 import { IAuthUser } from '../common/models/user/authUser';
 import { IRegisterUser } from '../common/models/user/registerUser';
 import { ITokenData } from '../common/models/jwt/tokenData';
 import { Roles } from '../common/enums/roles';
+
+export const getUserDataFromToken = (data: ITokenData): Promise<ITokenData> => Promise.resolve(data);
 
 export const login = async (user: IUser): Promise<IAuthUser> => {
   const { id } = user;
@@ -30,7 +32,6 @@ export const login = async (user: IUser): Promise<IAuthUser> => {
 
 export const register = async (user: IRegisterUser): Promise<IAuthUser> => {
   const { password, ...userData } = user;
-
   const newUser = await userRepository.addUser({
     ...userData,
     id: uuidv4(),
