c20f2fd532007d8e11d838ea7b4e63bd118761f3
rommyk <baron1xxx@gmail.com>
2021-03-05 16:49:29 +0200

    Add db connection to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { createConnection } from 'typeorm';
 import { env } from './env';
 import routes from './api/routes';
 
 const app = express();
+
+createConnection()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
