1cc557cfdc958d76af1ab181d0a4c5a88d020cd8
Anna <aniatychenko@gmail.com>
2021-03-05 19:53:14 +0200

    Implement apps scene

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -4,11 +4,12 @@ import { Routes } from '../../common/enums/Routes';
 import PrivateRoute from '../PrivateRoute';
 import PublicRoute from '../PublicRoute';
 import Resources from '../../scenes/Resources/index';
+import Apps from '../../scenes/Apps/index';
 
 const Routing: FunctionComponent = () => (
   <Switch>
-    <PrivateRoute path={Routes.Apps} component={() => <div>apps</div>} />
-    <PublicRoute path={Routes.Resources} component={Resources} />
+    <PrivateRoute path={Routes.Apps} component={Apps} />
+    <PrivateRoute path={Routes.Resources} component={Resources} />
     <PublicRoute path={Routes.SignIn} component={() => <div>sign in</div>} />
   </Switch>
 );
--- frontend/src/scenes/Apps/index.jsx
+++ frontend/src/scenes/Apps/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import { Navbar, Nav, Form, FormControl, Table } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import logo from '../images/retool-logo.png';
 import appsIcon from '../images/briefcase-solid.svg';
 import resourcesIcon from '../images/database-solid.svg';
 import { Routes } from '../../common/enums/Routes';
 
-const Resources = () => (
+const Apps = () => (
   <div className={styles['resources-wrp']}>
     <Navbar className={styles.navigation}>
 
@@ -16,11 +16,11 @@ const Resources = () => (
       </Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href={Routes.Apps}>
+          <Nav.Link active href={Routes.Apps}>
             <img src={appsIcon} className={styles['navbar-icons']} alt="apps-icon" />
             <span>Apps</span>
           </Nav.Link>
-          <Nav.Link active href={Routes.Resources}>
+          <Nav.Link href={Routes.Resources}>
             <img src={resourcesIcon} className={styles['navbar-icons']} alt="resources-icon" />
             <span>Resources</span>
           </Nav.Link>
@@ -29,55 +29,41 @@ const Resources = () => (
       </Navbar.Collapse>
 
     </Navbar>
-
     <div className={styles['main-block-wrp']}>
 
       <div className={styles['before-table']}>
-        <h1>Resources</h1>
+        <h1>All</h1>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="primary">Create new</Button>
         </Form>
       </div>
 
-      <div className="table-wrp">
-        <Table className="table table-hover">
-          <thead>
-            <tr>
-              <th scope="col">RESOURCE NAME</th>
-              <th scope="col">DATABASE TYPE</th>
-              <th scope="col">DATABASE NAME</th>
-              <th scope="col">CREATED</th>
-              <th scope="col"> </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>...</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>...</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-              <td>...</td>
-            </tr>
-          </tbody>
-        </Table>
+      <Form>
+        <Form.Check
+          type="checkbox"
+          label="Select all"
+        />
+      </Form>
+
+      <div className={styles['list-wrp']}>
+
+        <div className={styles['list-item']}>
+          <div className={styles['app-main-info']}>
+            <img src={appsIcon} className={styles['navbar-icons']} alt="apps-icon" />
+            <div className={styles['main-info']}>
+              <span>Workspaces and Teams</span>
+              <span className="text-secondary">Created by...</span>
+            </div>
+          </div>
+          <div>
+            <Button variant="outline-dark">...</Button>
+          </div>
+        </div>
       </div>
+
     </div>
   </div>
 );
 
-export default Resources;
+export default Apps;
