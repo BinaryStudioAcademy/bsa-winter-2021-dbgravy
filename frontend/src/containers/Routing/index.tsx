bf60556873b60ff592e00713f0649567d460bf64
loneliness221b <shrederrtop1@gmail.com>
2021-03-05 15:45:42 +0200

    delete BrowserRouter from Routing

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const Routing = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" />
-      <Route />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" />
+    <Route />
+  </Switch>
 );
 
 export default Routing;
