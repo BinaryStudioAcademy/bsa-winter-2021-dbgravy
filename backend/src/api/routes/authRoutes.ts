2b60b028d71ad9a3e27df759e4233199444006c8
Sergey <sergey_chizhik@ukr.net>
2021-03-05 10:06:42 +0200

    Update authRoutes.ts

--- backend/src/api/routes/authRoutes.ts
+++ backend/src/api/routes/authRoutes.ts
@@ -1,20 +1,19 @@
-import { Router } from 'express';
+import { Request, Router } from 'express';
 import { IRegisterUser } from '../../common/models/user/registerUser';
 import { IUser } from '../../common/models/user/user';
 import * as authService from '../../services/authService';
 import authenticationMiddleware from '../middlewares/authenticationMiddleware';
 import registrationMiddleware from '../middlewares/registrationMiddleware';
-// import jwtMiddleware from '../middlewares/jwtMiddleware';
+import jwtMiddleware from '../middlewares/jwtMiddleware';
+import { run } from '../../common/helpers/routeHelper';
+import { ITokenData } from '../../common/models/jwt/tokenData';
 
 const router: Router = Router();
 
 // user added to the request (req.user) in a strategy, see passport config
 router
-  .post('/sign-in', authenticationMiddleware, (req, res, next) => authService.login(req.user as IUser)
-    .then(data => res.send(data))
-    .catch(next))
-  .post('/sign-up', registrationMiddleware, (req, res, next) => authService.register(req.user as IRegisterUser)
-    .then(data => res.send(data))
-    .catch(next));
+  .post('/me', jwtMiddleware, run((req: Request) => authService.getUserDataFromToken(req.user as ITokenData)))
+  .post('/sign-in', authenticationMiddleware, run((req: Request) => authService.login(req.user as IUser)))
+  .post('/sign-up', registrationMiddleware, run((req: Request) => authService.register(req.user as IRegisterUser)));
 
 export default router;
