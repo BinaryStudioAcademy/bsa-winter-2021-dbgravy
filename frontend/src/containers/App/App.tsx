7b23f2832e73608c36fb04412e66d9953d66c443
loneliness221b <shrederrtop1@gmail.com>
2021-03-05 14:36:28 +0200

    fix Routing

---
+++ frontend/src/containers/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { store } from '../../store';
+import { Provider } from 'react-redux';
+import Routing from '../../containers/Routing';
+import { Route } from 'react-router';
+
+const App = () => (
+  <Provider store={store}>
+    <Route><Routing /></Route>
+  </Provider>
+);
+
+export default App;
