5c25be1fd03896a8a37e1a4b564a8ea6f88caedf
Sergey <sergey_chizhik@ukr.net>
2021-03-05 10:05:55 +0200

    Create errorHandlerMiddleware.ts

---
+++ backend/src/api/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from 'express';
+import { IError } from '../../common/models/common/error';
+
+export const errorHandlerMiddleware = (err: IError, _req: Request, res: Response, next: NextFunction) => {
+  if (res.headersSent) {
+    next(err);
+  } else {
+    const { status = 500, message = '' } = err;
+    res.status(status).send({ status, message });
+  }
+};
