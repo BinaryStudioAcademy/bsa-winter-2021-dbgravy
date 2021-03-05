daa21fd4a5619da3c6d88c0b0549bc9c264a51f2
Anna <aniatychenko@gmail.com>
2021-03-05 23:59:31 +0200

    Add routings to main pages

--- frontend/src/containers/Routing/index.tsx
+++ frontend/src/containers/Routing/index.tsx
@@ -1,15 +1,15 @@
 import React, { FunctionComponent } from 'react';
 import { Switch } from 'react-router-dom';
 import { Routes } from '../../common/enums/Routes';
-// import PrivateRoute from '../PrivateRoute';
+import PrivateRoute from '../PrivateRoute';
 import PublicRoute from '../PublicRoute';
 import Resources from '../../scenes/Resources/index';
 import Apps from '../../scenes/Apps/index';
 
 const Routing: FunctionComponent = () => (
   <Switch>
-    <PublicRoute path={Routes.Apps} component={Apps} />
-    <PublicRoute path={Routes.Resources} component={Resources} />
+    <PrivateRoute path={Routes.Apps} component={Apps} />
+    <PrivateRoute path={Routes.Resources} component={Resources} />
     <PublicRoute path={Routes.SignIn} component={() => <div>sign in</div>} />
   </Switch>
 );
