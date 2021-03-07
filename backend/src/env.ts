f0414dffe910862e2af544ba3fb2bd62c9f26171
Alexey <th3krp@gmail.com>
2021-03-07 13:21:57 +0300

    added user organization api

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -6,5 +6,9 @@ dotenv.config();
 export const env = {
   app: {
     port: getOsEnv('PORT') || 3001
+  },
+  sendGrid: {
+    apiKey: process.env.SENDGRID_API_KEY,
+    emailFrom: process.env.SENDGRID_EMAIL_FROM
   }
 };
