2a7520194a629bb71bdeacd7e8f1003e26d01fb6
Sergey <sergey_chizhik@ukr.net>
2021-03-07 20:23:11 +0200

    Move /me to userRoute

--- backend/src/api/routes/authRoutes.ts
+++ backend/src/api/routes/authRoutes.ts
@@ -3,9 +3,7 @@ import { IRegisterUser } from '../../common/models/user/IRegisterUser';
 import * as authService from '../../services/authService';
 import authenticationMiddleware from '../middlewares/authenticationMiddleware';
 import registrationMiddleware from '../middlewares/registrationMiddleware';
-import jwtMiddleware from '../middlewares/jwtMiddleware';
 import { run } from '../../common/helpers/routeHelper';
-import { ITokenData } from '../../common/models/tokens/ITokenData';
 import refreshTokenMiddleware from '../middlewares/refreshTokenMiddleware';
 import { ITransportedUser } from '../../common/models/user/ITransportedUser';
 
@@ -13,7 +11,6 @@ const router: Router = Router();
 
 // user added to the request (req.user) in a strategy, see passport config
 router
-  .post('/me', jwtMiddleware, run((req: Request) => authService.getUserDataFromToken(req.user as ITokenData)))
   .post('/token', refreshTokenMiddleware, run((req: Request) => authService.refreshToken(req.user as ITransportedUser)))
   .post('/sign-in', authenticationMiddleware, run((req: Request) => authService.login(req.user as ITransportedUser)))
   .post('/sign-up', registrationMiddleware, run((req: Request) => authService.register(req.user as IRegisterUser)));
