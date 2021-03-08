66d1aeaaf58848caa7f49b1ebaad30ef1accb014
rommyk <baron1xxx@gmail.com>
2021-03-08 10:48:01 +0200

    Connect apps service functions with DB

--- backend/src/services/applicationService.ts
+++ backend/src/services/applicationService.ts
@@ -1,40 +1,56 @@
-const apps = [
-  {
-    id: '1',
-    name: 'App1',
-    organizationId: '1',
-    updatedByUserId: '1'
-  },
-  {
-    id: '2',
-    name: 'App2',
-    organizationId: '2',
-    updatedByUserId: '2'
+import { getCustomRepository } from 'typeorm';
+import { ApplicationRepository } from '../data/repositories/applicationRepository';
+
+export const getApps = async () => {
+  try {
+    const apps = await getCustomRepository(ApplicationRepository).getAllApp();
+    if (apps.length === 0) {
+      throw new Error('Apps not found');
+    }
+    return apps;
+  } catch (e) {
+    throw new Error(e.message);
   }
-];
-
-export const getApps = () => Promise.resolve(apps);
-
-export const getAppById = (id: string) => Promise.resolve(apps.find(a => a.id === id));
-
-export const addApp = ({ appName }: any) => {
-  const appId = new Date().getTime().toString();
-  const name: string = appName;
-  const app = { id: appId, name, organizationId: `o${appId}`, updatedByUserId: `u${appId}` };
-  apps.push(app);
-  return Promise.resolve(app);
 };
 
-export const updateApp = (id:string, appName: string) => new Promise(resolve => {
-  apps.forEach(app => {
-    if (app.id === id) {
-      const updated = { ...app, name: appName };
-      resolve(updated);
+export const getAppById = async (id: string) => {
+  try {
+    const app = await getCustomRepository(ApplicationRepository).getAppById(id);
+    if (app) {
+      throw new Error('App not found');
     }
-  });
-});
+    return app;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
 
-export const deleteApp = (id: string) => new Promise(resolve => {
-  apps.filter(a => a.id !== id);
-  resolve(id);
-});
+export const addApp = async ({ name, organizationId, updatedByUserId }: any) => {
+  const app = await getCustomRepository(ApplicationRepository).addApp(
+    {
+      name,
+      organizationId,
+      updatedByUserId
+    }
+  );
+  return app;
+};
+
+export const updateApp = async (id: string, name: string) => {
+  await getAppById(id);
+  const editedApp = await getCustomRepository(ApplicationRepository).updateApp(id, { name });
+  return editedApp;
+};
+
+export const deleteApp = async (id: string) => {
+  try {
+    await getAppById(id);
+    const deletedApp = await getCustomRepository(ApplicationRepository).deleteApp(id);
+    if (deletedApp) {
+      throw new Error('App not deleted');
+    }
+    return deletedApp;
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
