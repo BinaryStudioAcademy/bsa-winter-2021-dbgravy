7b435cc104e4293e3e4a101b34c5502ada10a07c
Anna <aniatychenko@gmail.com>
2021-03-09 12:59:36 +0200

    Change routes

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -2,7 +2,6 @@ import { Express } from 'express';
 import userRoute from './userRoute';
 import authRoute from './authRoute';
 import applicationRoute from './applicationRoute';
-// import userOrganizationRoute from './userOrganizationRout';
 import resourceRoutes from './resourcesRoute';
 import userOrganizationRoute from './userOrganizationRoute';
 
@@ -10,7 +9,6 @@ const routes = (app: Express) => {
   app.use('/api/users', userRoute);
   app.use('/api/user/organization', userOrganizationRoute);
   app.use('/api/applications', applicationRoute);
-  // app.use('/api/auth', authRoutes);
   app.use('/api/resources', resourceRoutes);
   app.use('/api/auth', authRoute);
 };
