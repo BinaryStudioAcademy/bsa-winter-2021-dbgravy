310f2c4266c6b023e20d7954d70c379158bbe3e7
Anton Shevchenko <korolewky.anton3@gmail.com>
2021-03-14 12:29:25 +0200

    feat(fr): added controls to appItem

--- frontend/src/scenes/Apps/components/AppItem/index.tsx
+++ frontend/src/scenes/Apps/components/AppItem/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { IApps } from '../../../../common/models/apps/IApps';
 import Moment from 'react-moment';
 import { CalendarEnum } from '../../../Resources/enums/CalendarEnum';
@@ -13,26 +13,54 @@ interface IProps {
   deleteApp: (data: { app: IApps }) => void;
   showEdit: (data: { app: IApps, show: boolean }) => void;
 }
-
 const AppItem: React.FC<IProps> = ({
   app, showEdit, deleteApp
-}) => (
-  <div className={styles['list-item']}>
-    <div className={styles['app-main-info']}>
-      <FontAwesomeIcon icon={faBriefcase} />
-      <div className={styles['main-info']}>
-        <span>{app.name}</span>
-        <span className="text-secondary">
-          Created&ensp;
-          <Moment calendar={CalendarEnum}>{app.createdAt}</Moment>
-        </span>
+}) => {
+  const [display, setDisplay] = useState(false);
+
+  const onEdit = () => {
+    showEdit({ app, show: true });
+    setDisplay(!display);
+  };
+
+  return (
+    <div className={styles['list-item']}>
+      <div className={styles['app-main-info']}>
+        <FontAwesomeIcon icon={faBriefcase} />
+        <div className={styles['main-info']}>
+          <span>{app.name}</span>
+          <span className="text-secondary">
+            Created&ensp;
+            <Moment calendar={CalendarEnum}>{app.createdAt}</Moment>
+          </span>
+        </div>
+      </div>
+      <div>
+        <Button variant="dark" onClick={() => setDisplay(!display)}>
+          <FontAwesomeIcon icon={faCaretDown} color="white" />
+        </Button>
+        <div className={`${styles.child} ${display ? styles.none : ''}`}>
+          <span
+            onClick={() => onEdit()}
+            role="button"
+            onKeyPress={() => onEdit()}
+            tabIndex={0}
+          >
+            Edit
+          </span>
+          <span
+            onClick={() => deleteApp({ app })}
+            role="button"
+            onKeyPress={() => deleteApp({ app })}
+            tabIndex={0}
+          >
+            Delete
+          </span>
+        </div>
+        <UpdateApp />
       </div>
     </div>
-    <div>
-      <Button variant="outline-dark">...</Button>
-    </div>
-    <UpdateApp />
-  </div>
-);
+  );
+};
 
 export default AppItem;
