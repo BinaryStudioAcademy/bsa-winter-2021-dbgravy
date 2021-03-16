ac27bf05f6094a9704535ab798d18f23a6f9073a
rommyk <baron1xxx@gmail.com>
2021-03-16 12:48:25 +0200

    Changed expire time of jwt token

--- backend/src/config/jwtConfig.ts
+++ backend/src/config/jwtConfig.ts
@@ -3,4 +3,4 @@ import { env } from '../env';
 export const { secret } = env.app;
 export const expiresAccessTokenIn = 86400;
 export const expiresRefreshTokenIn = '28 days';
-export const expiresInviteTokenIn = '1h';
+export const expiresInviteTokenIn = '1d';
