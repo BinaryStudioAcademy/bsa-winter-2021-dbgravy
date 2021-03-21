1512dde0ed0595e72e834e0bbb21ffaac77484d1
rommyk <baron1xxx@gmail.com>
2021-03-21 13:57:41 +0200

    Add fetchUserOrganizationsRoutine and changeUserOrganizationRoutines

--- frontend/src/containers/ProfilePopup/routines.ts
+++ frontend/src/containers/ProfilePopup/routines.ts
@@ -2,3 +2,5 @@ import { createRoutine } from 'redux-saga-routines';
 
 export const fetchOrgInfoRoutine = createRoutine('FETCH_ORG_INFO');
 export const createOrganizationRoutine = createRoutine('CREATE_ORGANIZATION');
+export const fetchUserOrganizationsRoutine = createRoutine('FETCH_USER_ORGANIZATIONS');
+export const changeUserOrganizationRoutine = createRoutine('CHANGE_USER_ORGANIZATION');
