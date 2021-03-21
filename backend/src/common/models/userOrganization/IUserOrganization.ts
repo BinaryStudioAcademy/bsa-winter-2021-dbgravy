f3a08625d31b93e8257713a4d5f44e142eb25ce8
rommyk <baron1xxx@gmail.com>
2021-03-21 13:54:14 +0200

    Add organization field to IUserOrganization interface

--- backend/src/common/models/userOrganization/IUserOrganization.ts
+++ backend/src/common/models/userOrganization/IUserOrganization.ts
@@ -1,12 +1,14 @@
 import { Role } from '../../enums/Role';
 import { OrganizationStatus } from '../../enums/OrganizationStatus';
 import { IUserFields } from './IUserFields';
+import { IOrganization } from '../organization/IOrganization';
 
 export interface IUserOrganization {
   id: string,
   organizationId?: string,
   role: Role,
   status: OrganizationStatus,
-  user: IUserFields
+  user: IUserFields,
+  organization?: IOrganization,
 }
 
