fbd52b958c361052e514154ec9254593af0e8049
loneliness221b <shrederrtop1@gmail.com>
2021-03-23 17:18:16 +0200

    fix checkbox

--- frontend/src/scenes/constructor/containers/index.tsx
+++ frontend/src/scenes/constructor/containers/index.tsx
@@ -310,34 +310,32 @@ const Constructor: React.FC<IProps> = ({ id }) => {
             <Form.Label className={style.row} />
             <Form.Label className={style.row} />
             <div className={style.checkBoxWrapper}>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <div
                 onClick={changeConfirm}
                 onKeyDown={changeConfirm}
                 role="button"
                 tabIndex={0}
                 className={style.Confirm}
-              >
-                {
+              />
+              {
               !query.setNewConfirm ? (
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M5 2c-1.654 0-3 1.346-3 3v14c0 1.654 1.346 3
-                     3 3h14c1.654 0 3-1.346 3-3v-14c0-1.654-1.346-3-3-3h-14zm19
-                     3v14c0 2.761-2.238 5-5 5h-14c-2.762 0-5-2.239-5-5v-14c0-2.761
-                      2.238-5 5-5h14c2.762 0 5 2.239 5 5z"
-                  />
-                </svg>
+                <Form.Check
+                  type="checkbox"
+                  id="checkbox"
+                  className={style.checkBox}
+                  disabled
+                />
               )
                 : (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M10.041 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418
-                     8.591zm-5.041-15c-1.654 0-3 1.346-3 3v14c0 1.654 1.346 3 3 3h14c1.654 0
-                     3-1.346 3-3v-14c0-1.654-1.346-3-3-3h-14zm19 3v14c0 2.761-2.238 5-5
-                     5h-14c-2.762 0-5-2.239-5-5v-14c0-2.761 2.238-5 5-5h14c2.762 0 5 2.239 5 5z"
-                    />
-                  </svg>
+                  <Form.Check
+                    type="checkbox"
+                    id="checkbox2"
+                    className={style.checkBox}
+                    checked
+                  />
                 )
             }
-              </div>
               <span className={style.spanText}>Show a confirmation modal before running</span>
             </div>
             <Form.Label className={style.row} />
