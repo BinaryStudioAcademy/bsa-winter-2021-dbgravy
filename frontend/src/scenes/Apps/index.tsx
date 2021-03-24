144bc0ff2a72f9366295dc9cad8c4229aa0e9d49
Anton Shevchenko <korolewky.anton3@gmail.com>
2021-03-24 16:57:24 +0200

    not a fix

--- frontend/src/scenes/Apps/index.tsx
+++ frontend/src/scenes/Apps/index.tsx
@@ -28,7 +28,6 @@ const Apps: React.FC<IProps> = ({ fetchApps, addApp, apps, isLoading, deleteApp,
   }, []);
 
   const [searchValue, setSearchValue] = useState<string>('');
-  const [focused, setFocused] = useState(false);
 
   return (
     <div className={styles['apps-wrp']}>
@@ -40,9 +39,7 @@ const Apps: React.FC<IProps> = ({ fetchApps, addApp, apps, isLoading, deleteApp,
             <Form inline>
               <InputGroup>
                 <InputGroup.Prepend>
-                  <InputGroup.Text className={[styles['input-group-search'], `${focused
-                    ? styles.focused : ''}`].join(' ')}
-                  >
+                  <InputGroup.Text className={styles['input-group-search']}>
                     <FontAwesomeIcon icon={faSearch} />
                   </InputGroup.Text>
                 </InputGroup.Prepend>
@@ -54,8 +51,6 @@ const Apps: React.FC<IProps> = ({ fetchApps, addApp, apps, isLoading, deleteApp,
                   type="text"
                   placeholder="Search"
                   className={`mr-sm-2 border-left-0 ${styles['form-control-search']}`}
-                  onFocus={() => setFocused(true)}
-                  onBlur={() => setFocused(false)}
                 />
               </InputGroup>
               <AddApp onAddApp={name => addApp(name)} />
